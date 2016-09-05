01532b002f2263d7592d1da3e235b140b8e7ccce
Zhenbo Li <litimetal@gmail.com>
2016-09-05 10:58:24 +0800

    在触发后才会开始创建对象

--- mtg_buy.js
+++ mtg_buy.js
@@ -9,38 +9,7 @@
 // ==/UserScript==
 
 class MTG_BUYER_CLASS {
-    constructor() {
-        this.mainDiv = document.createElement("div");
-        this.mainDiv.setAttribute("id", "mtgCarManager");
-
-        this.shopAmount = 3;
-        this.createForm = function(shop) {
-            var form = document.createElement("form");
-            form.setAttribute("id", "mtgCarInputArea");
-
-            for (var i=0; i<shop; i++) {
-                var inp = document.createElement("input");
-                inp.type="text";
-                inp.setAttribute("id", "mtgCarShopLink"+String(i));
-                inp.setAttribute("type", "text");
-                form.appendChild(inp);
-            }
-
-            form.appendChild( document.createElement("br") );
-            form.appendChild( document.createTextNode("输入单卡名称") );
-            form.appendChild( document.createElement("br") );
-
-            var btn = document.createElement("button");
-            btn.appendChild( document.createTextNode("Search") );
-            btn.id = "mtgSubmitButton";
-            btn.type = "button";
-            btn.onclick=function(e) {alert("click");submitForm(e);return false;}
-            form.appendChild(btn);
-            return form;
-        }
-
-        this.mainDiv.appendChild(this.createForm(this.shopAmount));
-    }
+    constructor() {}
     /*
     submitForm(e) {
         alert("B");
@@ -50,8 +19,41 @@ class MTG_BUYER_CLASS {
 
 };
 MTG_BUYER_CLASS.prototype.start = function() {
-        $(".J_Cover").append(this.mainDiv);
-        return false;
+    this.submitForm = function(e) {alert("Stub here"); return false;}
+
+    this.mainDiv = document.createElement("div");
+    this.mainDiv.setAttribute("id", "mtgCarManager");
+
+    this.shopAmount = 3;
+    this.createForm = function(shop) {
+        var form = document.createElement("form");
+        form.setAttribute("id", "mtgCarInputArea");
+
+        for (var i=0; i<shop; i++) {
+            var inp = document.createElement("input");
+            inp.type="text";
+            inp.setAttribute("id", "mtgCarShopLink"+String(i));
+            inp.setAttribute("type", "text");
+            form.appendChild(inp);
+        }
+
+        form.appendChild( document.createElement("br") );
+        form.appendChild( document.createTextNode("输入单卡名称") );
+        form.appendChild( document.createElement("br") );
+
+        var btn = document.createElement("button");
+        btn.appendChild( document.createTextNode("Search") );
+        btn.id = "mtgSubmitButton";
+        btn.type = "button";
+        btn.onclick=function(e) {alert("click");this.submitForm(e);return false;}
+        form.appendChild(btn);
+        return form;
+    }
+
+    this.mainDiv.appendChild(this.createForm(this.shopAmount));
+
+    $(".J_Cover").append(this.mainDiv);
+    return false;
 }
 
 /*var MTG_BUYER = Object.create(MTG_BUYER_CLASS);*/
