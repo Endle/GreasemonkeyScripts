d59e0627acc741ec9abe3c2db2468bbfd38de20c
Zhenbo Li <litimetal@gmail.com>
2016-09-05 11:44:08 +0800

    submitForm() 可以正确读取表格信息

--- mtg_buy.js
+++ mtg_buy.js
@@ -14,7 +14,15 @@ class MTG_BUYER_CLASS {
 var MTG_BUYER = new MTG_BUYER_CLASS();
 
 MTG_BUYER_CLASS.prototype.submitForm = function(e) {
-        alert("submit");
+        var shops = new Array(this.shopAmount);
+        var cards = new Array(this.cardAmount);
+
+        for (var i=0; i<this.shopAmount; i++) {
+            shops[i] = $("#mtgCarShopLink"+String(i)).val();
+        }
+        for (var i=0; i<this.cardAmount; i++) {
+            cards[i] = $("#mtgCarCardName"+String(i)).val();
+        }
         return false;
 }
 
@@ -22,8 +30,9 @@ MTG_BUYER_CLASS.prototype.start = function() {
     this.mainDiv = document.createElement("div");
     this.mainDiv.setAttribute("id", "mtgCarManager");
 
-    this.shopAmount = 3;
-    this.createForm = function(shop) {
+    this.shopAmount = 2;
+    this.cardAmount = 4;
+    this.createForm = function(shop, card) {
         var form = document.createElement("form");
         form.setAttribute("id", "mtgCarInputArea");
 
@@ -39,6 +48,15 @@ MTG_BUYER_CLASS.prototype.start = function() {
         form.appendChild( document.createTextNode("输入单卡名称") );
         form.appendChild( document.createElement("br") );
 
+        for (var i=0; i<card; i++) {
+            var inp = document.createElement("input");
+            inp.type="text";
+            inp.setAttribute("id", "mtgCarCardName"+String(i));
+            inp.setAttribute("type", "text");
+            form.appendChild(inp);
+        }
+
+        form.appendChild( document.createElement("br") );
         var btn = document.createElement("button");
         btn.appendChild( document.createTextNode("Search") );
         form.appendChild(btn);
@@ -48,7 +66,7 @@ MTG_BUYER_CLASS.prototype.start = function() {
         return form;
     }
 
-    this.mainDiv.appendChild(this.createForm(this.shopAmount));
+    this.mainDiv.appendChild(this.createForm(this.shopAmount, this.cardAmount));
 
     $(".J_Cover").append(this.mainDiv);
     return false;
